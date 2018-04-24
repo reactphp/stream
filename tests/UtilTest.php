9557f7d7fcda8d3abc1362994259d8702ebaf589
Níckolas Daniel da Silva <nickolas@phpsp.org.br>
2018-04-24 12:17:36 +0200

    Removes unused private method from test

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -266,9 +266,4 @@ class UtilTest extends TestCase
     {
         return $this->getMockBuilder('React\EventLoop\LoopInterface')->getMock();
     }
-
-    private function notEqualTo($value)
-    {
-        return new \PHPUnit_Framework_Constraint_Not($value);
-    }
 }
