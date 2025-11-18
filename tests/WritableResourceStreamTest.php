4e998a50a6ec8ccd52ff2078a938abb0058712e5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-11-18 17:14:47 +0100

    [1.x] Fix reflection setAccessible deprecatoin warnings
    In PHP8.1 ReflectionProperty::setAccessible was made a no-op through https://wiki.php.net/rfc/make-reflection-setaccessible-no-op in PHP8.5 it is now throwing a deprecation warning by: https://wiki.php.net/rfc/deprecations_php_8_5#deprecate_reflectionsetaccessible

--- tests/WritableResourceStreamTest.php
+++ tests/WritableResourceStreamTest.php
@@ -26,7 +26,9 @@ class WritableResourceStreamTest extends TestCase
         $stream = new WritableResourceStream($resource);
 
         $ref = new \ReflectionProperty($stream, 'loop');
-        $ref->setAccessible(true);
+        if (PHP_VERSION_ID < 80100) {
+            $ref->setAccessible(true);
+        }
         $loop = $ref->getValue($stream);
 
         $this->assertInstanceOf('React\EventLoop\LoopInterface', $loop);
